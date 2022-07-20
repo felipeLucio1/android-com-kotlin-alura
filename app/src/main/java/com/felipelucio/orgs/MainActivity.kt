33ee879cada6565376006c30c631d6e228fd1f96
felipe lucio <felipelucio2009@gmail.com>
2022-07-19 23:58:18 -0300

    feat: adiciona layout a activity

--- app/src/main/java/com/felipelucio/orgs/MainActivity.kt
+++ app/src/main/java/com/felipelucio/orgs/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.felipelucio.orgs
 
 import android.app.Activity
-import android.content.Context
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val textView = TextView(this)
-        textView.setText("Cesta de frutas")
-        setContentView(textView)
+        setContentView(R.layout.activity_main)
     }
 }
