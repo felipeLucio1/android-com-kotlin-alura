94a9648e35948e824d2e0e203016023cf0191de2
felipe lucio <felipelucio2009@gmail.com>
2022-07-19 23:34:40 -0300

    feat: adiciona textview na activity

--- app/src/main/java/com/felipelucio/orgs/MainActivity.kt
+++ app/src/main/java/com/felipelucio/orgs/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.felipelucio.orgs
 
 import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Bem vindo(a) ao Orgs!", Toast.LENGTH_LONG).show()
+
+        val textView = TextView(this)
+        textView.setText("Cesta de frutas")
+        setContentView(textView)
     }
 }
