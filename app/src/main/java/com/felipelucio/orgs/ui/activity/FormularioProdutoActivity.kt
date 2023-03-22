da43ffb24cad7cdf8aba8c1c9309e0830252f12b
Felipe Lucio do Nascimento <felipelucio2009@gmail.com>
2023-03-21 22:28:28 -0300

    feat: cria activity de formulário e adiciona/modifica versoes de libs no gradle (#5)
    * feat: cria activity formulario de produtos
    
    * feat: muda heranca da MainActivity para ser de AppCompatActivity
    
    * feat: modifica versoes de libs

---
+++ app/src/main/java/com/felipelucio/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,12 @@
+package com.felipelucio.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.felipelucio.orgs.R
+
+class FormularioProdutoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_produto)
+    }
+}
