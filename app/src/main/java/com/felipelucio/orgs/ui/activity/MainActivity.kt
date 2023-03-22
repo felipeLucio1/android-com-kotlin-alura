00e03bae765cda65f6162efd74ca18583743f59e
felipe lucio <felipelucio2009@gmail.com>
2023-03-21 21:54:46 -0300

    feat: muda heranca da MainActivity para ser de AppCompatActivity

--- app/src/main/java/com/felipelucio/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/felipelucio/orgs/ui/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.felipelucio.orgs.ui.activity
 import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.felipelucio.orgs.R
@@ -10,11 +11,9 @@ import com.felipelucio.orgs.model.Produto
 import com.felipelucio.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import java.math.BigDecimal
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_main)
 
         val listaProdutos = findViewById<RecyclerView>(R.id.lista_produtos)
         listaProdutos.adapter = ListaProdutosAdapter(this,
