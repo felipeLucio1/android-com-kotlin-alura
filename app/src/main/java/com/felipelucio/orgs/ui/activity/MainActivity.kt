402d92cedc17c28e583f8c4464965e63066a1288
felipe lucio <felipelucio2009@gmail.com>
2023-09-21 08:27:56 -0300

    feat: implementa chamada a tela de formulario

--- app/src/main/java/com/felipelucio/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/felipelucio/orgs/ui/activity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.felipelucio.orgs.ui.activity
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.felipelucio.orgs.R
 import com.felipelucio.orgs.model.Produto
 import com.felipelucio.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
@@ -35,5 +37,11 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                 ),
             )
         )
+
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener {
+            val intent = Intent(this, FormularioProdutoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
